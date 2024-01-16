db1bea5482b244b22980701c758450324200c54a
NachoMuslera <nmusleramosquera@gmail.com>
2024-01-16 13:41:50 +0100

    Ejercicio1

---
+++ EjerciciosEntregables_IgnacioMusleraMosquera71735888S/DNI.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EjercicioDNI</title>
+</head>
+<body>
+    <?php
+    $personas = [
+
+        "71735888S" => [
+            "nombre" => "Ignacio",
+            "apellido1" => "Muslera",
+            "apellido2" => "Mosquera"
+        ],
+
+        "4567891P" => [
+            "nombre" => "Dudu",
+            "apellido1" => "Padre",
+            "apellido2" => "Rap"
+        ],
+
+        "9876543K" => [
+            "nombre" => "Eustaquio",
+            "apellido1" => "Abichuela",
+            "apellido2" => "González"
+        ],
+
+        "3332221J" => [
+            "nombre" => "Julián",
+            "apellido1" => "Álvarez",
+            "apellido2" => "Pérez"
+        ],
+
+        "12345678L" => [
+            "nombre" => "Manuel",
+            "apellido1" => "Cortés",
+            "apellido2" => "Rey"
+        ],
+
+        "7776669S" => [
+            "nombre" => "Luffy",
+            "apellido1" => "Monkey",
+            "apellido2" => "D"
+        ]
+    ];
+?>
+</body>
+</html>
